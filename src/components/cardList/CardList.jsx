bdb4065430a99f92d68de5fa73503f887e310ac9
Naveed Ali <advmiannaveedali@gmail.com>
2024-11-18 18:20:08 +0500

    add  cardlist

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+import Image from "next/image";
+import Card from "../card/Card";
 
-const CardList = () => {
+// const getData = async (page, cat) => {
+//   const res = await fetch(
+//     `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+//     {
+//       cache: "no-store",
+//     }
+//   );
+
+//   if (!res.ok) {
+//     throw new Error("Failed");
+//   }
+
+//   return res.json();
+// };
+
+const CardList = async () => {
+
   return (
-    <div>CardList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+     <Card/>
+     <Card/>
+     <Card/>
+      </div>
+    </div>
+  );
+};
 
-export default CardList
+export default CardList;

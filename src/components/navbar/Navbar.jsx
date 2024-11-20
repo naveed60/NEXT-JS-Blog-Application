026eabe67c42f617e4fc1a312f2461943dead19f
Naveed Ali <advmiannaveedali@gmail.com>
2024-11-20 14:55:08 +0500

    fix navbar issues

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,13 +8,10 @@ import ThemeToggle from "../themeToggle/ThemeToggle";
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.social}>
-        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
-        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
-        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
-        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
-      </div>
       <div className={styles.logo}>Naveed Writes</div>
+      <div className={styles.social}>
+        
+      </div>
       <div className={styles.links}>
         <ThemeToggle />
         <Link href="/" className={styles.link}>Homepage</Link>
